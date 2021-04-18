72d8c4f4e9c8f9511fe119401c49cf471ad754c2
Mathis Le Bonniec <mathislbonniec@gmail.com>
2021-04-19 00:14:54 +0200

    🎨 Improve structure in adding controllers

---
+++ src/controllers/index.ts
@@ -0,0 +1,52 @@
+import languages from '@helpers/languages';
+import Github from '@models/github';
+import Generator from '@helpers/generator';
+import type { components } from '@octokit/openapi-types'
+import type { FastifyReply, FastifyRequest, RequestGenericInterface } from 'fastify';
+
+interface RepositoryParams extends RequestGenericInterface {
+	Params: {
+		owner: string;
+		repo: string;	
+	}
+}
+
+export async function getRepository(request: FastifyRequest<RepositoryParams>, reply: FastifyReply) {
+	const { owner, repo } = request.params;
+	const gh = new Github(owner, repo);
+
+	const { 
+		name,
+		owner: _user,
+		description,
+		stargazers_count: stars,
+		open_issues: issues,
+		forks_count: forks,
+	} = await gh.getRepository();
+	
+	// TODO: Fix Github / TS typing error
+	const user = _user as Exclude<components['schemas']['simple-user'], null>
+	
+	// TODO: Request all other pages, because it returns only 30 contributors per page
+	// const contributors = await gh.getContributorsCount();
+	const languagesList = await gh.getLanguages();
+	const avatar = await gh.getAvatar(user);
+
+	const img = new Generator({
+		author: user.login,
+		repository: name,
+		description: description || '',
+		languages: languages(languagesList),
+		avatar,
+		data: {
+			// contributors,
+			issues,
+			forks,
+			stars
+		}
+	});
+	await img.init();
+
+	reply.type('image/png');
+	reply.send(img.canvas.toBuffer());
+}
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,54 +1,6 @@
-import type { FastifyInstance, FastifyServerOptions, RequestGenericInterface } from 'fastify';
-import languages from '@helpers/languages';
-import Github from '@models/github';
-import Generator from '@controllers/generator';
-import type { components } from '@octokit/openapi-types'
-
-interface RepositoryParams extends RequestGenericInterface {
-	Params: {
-		owner: string;
-		repo: string;	
-	}
-}
+import { getRepository } from '@controllers/index';
+import type { FastifyInstance, FastifyServerOptions } from 'fastify';
 
 export default async function (app: FastifyInstance, _opts: FastifyServerOptions): Promise<void> {
-	app.get<RepositoryParams>('/:owner/:repo', async (request, reply) => {
-		const { owner, repo } = request.params;
-		const gh = new Github(owner, repo);
-
-		const { 
-			name,
-			owner: _user,
-			description,
-			stargazers_count: stars,
-			open_issues: issues,
-			forks_count: forks,
-		} = await gh.getRepository();
-		
-		// TODO: Fix Github / TS typing error
-		const user = _user as Exclude<components['schemas']['simple-user'], null>
-		
-		// TODO: Request all other pages, because it returns only 30 contributors per page
-		// const contributors = await gh.getContributorsCount();
-		const languagesList = await gh.getLanguages();
-		const avatar = await gh.getAvatar(user);
-
-		const img = new Generator({
-			author: user.login,
-			repository: name,
-			description: description || '',
-			languages: languages(languagesList),
-			avatar,
-			data: {
-				// contributors,
-				issues,
-				forks,
-				stars
-			}
-		});
-		await img.init();
-
-		reply.type('image/png');
-		reply.send(img.canvas.toBuffer());
-	});
+	app.get('/:owner/:repo', getRepository);
 };
